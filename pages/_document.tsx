134e38e13ef1ffebae1bd785b4d39115571cf3cd
JanSzewczyk <jan.szewczyk1997@gmail.com>
2022-10-18 14:31:13 +0200

    fix: fix font family link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,12 @@ function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/*<link rel="preconnect" href="https://fonts.googleapis.com" />*/}
-        {/*<link rel="preconnect" href="https://fonts.gstatic.com" />*/}
-        {/*<link*/}
-        {/*  href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap"*/}
-        {/*  rel="stylesheet"*/}
-        {/*/>*/}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body className="antialiased">
         <Main />
