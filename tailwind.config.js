e7f1e30d79b68308be5f7643baad39b41d8fb641
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-02-27 12:11:29 +0100

    refactor: update `@szum-tech/design-system` package to latest version

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@szum-tech/design-system"
+    "./node_modules/@szum-tech/design-system/{components,hooks,contexts}/**/*{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {}
   },
   plugins: [],
-  presets: [require("@szum-tech/design-system/tailwindcss/main-preset")]
+  presets: [require("@szum-tech/design-system/theme/main-preset")]
 };
